236b13a21b374c08228fcdd456be5e02ea0f5714
Paras Tomar <parastomar851@gmail.com>
2025-04-15 22:24:28 +0530

    Solved Question8.java

--- Question1.java
+++ Question1.java
@@ -1,6 +1,6 @@
 //   Question 2 : Write a program to find the sum of elements in a given range [L, R] using a prefix sum array.
     import java.util.Scanner;
-    public class Question_1 {
+    public class Question1 {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             System.out.print("Enter size of array: ");
