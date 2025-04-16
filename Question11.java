36e9582b2cce180c93243df5b1aee281d237fec1
Paras Tomar <parastomar851@gmail.com>
2025-04-16 08:44:53 +0530

    Solved Question11.java

---
+++ Question11.java
@@ -0,0 +1,27 @@
+public class Question11 {
+    public static int[] twoSum(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == target) {
+                return new int[]{left, right};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        if (result[0] != -1) {
+            System.out.println("Numbers found at indices: " + result[0] + " and " + result[1]);
+        } else {
+            System.out.println("No solution found.");
+        }
+    }
+}
