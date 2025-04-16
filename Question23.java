60fe5eafd6f583431550d1a39b7ab71ad74d170c
Paras Tomar <parastomar851@gmail.com>
2025-04-16 09:25:09 +0530

    Solved Question19

---
+++ Question23.java
@@ -0,0 +1,19 @@
+public class Question23 {
+
+    public static int findMaximumXOR(int[] nums) {
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int xor = nums[i] ^ nums[j];
+                if (xor > max) {
+                    max = xor;
+                }
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int[] nums = {3, 10, 5, 25, 2, 8};
+        System.out.println("Maximum XOR: " + findMaximumXOR(nums));
+    }
+}
